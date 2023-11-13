52a9fed072ed79105572e7b245edd264276aa379
Khayal <khayalmans994@gmail.com>
2023-11-13 22:59:57 +0400

    the end 2

---
+++ app.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal(".about-col-1", {delay: 400})
+ScrollReveal().reveal(".sub-titel", {delay: 550})
+ScrollReveal().reveal(".p", {delay: 650})
+ScrollReveal().reveal(".aboutme", {delay: 750})
+ScrollReveal().reveal(".icon", {delay: 850})
+
+

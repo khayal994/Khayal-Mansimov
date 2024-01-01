db05164a0ae18c3e3507a7bbad5f8f43353ed2a6
Khayal <khayalmans994@gmail.com>
2024-01-01 23:10:33 +0400

    demo

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ ScrollReveal().reveal(".sub-titel", {delay: 550})
 ScrollReveal().reveal(".p", {delay: 650})
 ScrollReveal().reveal(".aboutme", {delay: 750})
 ScrollReveal().reveal(".icon", {delay: 850})
+ScrollReveal().reveal(".expertiesH1", {delay: 950})
+ScrollReveal().reveal(".HTML", {delay: 1050})
+ScrollReveal().reveal(".CSS", {delay: 1100})
+ScrollReveal().reveal(".Boot", {delay: 1200})
+ScrollReveal().reveal(".Git", {delay: 1300})
+
 
 
